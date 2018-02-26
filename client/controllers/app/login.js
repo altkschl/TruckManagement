89d9231a00b1f283fac0b3b4415aec0b33df7fa4
Kevin Schlosser <kevin.schlosser@flex.com>
2018-02-26 06:34:25 -0800

    [Studio] Modified screen "login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -2,17 +2,13 @@ angular.module('app').controller('app_login', app_login);
 function app_login($scope, app, $q, powwowLoginNew) {
     'use strict';
     app.init($scope); 
-    console.log("11111111111");
     $scope.login = function () {
      $scope.app.showLoading('Logging in');    
     var credentials = {'username': $scope.data.username, 'password': $scope.data.password};
-    console.log("222222222222222");
     app.call('login.loginBasic', credentials);
-    console.log("3333333333333");
     }; 
     app.origEstablishConnection = app.establishConnection;
     app.establishConnection = function (params) {
-        console.log("44444444444");
         if (app.alreadyConnected) {
             console.log("Calling powwowLoginNew getcachedcredentials");
             var credentials = powwowLoginNew.getCachedCredentials();
